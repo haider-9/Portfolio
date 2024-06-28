bc459610c00b8ad85ac0b7298b32c3fd7fcff231
Haider Ahmad <155129917+haider-9@users.noreply.github.com>
2024-06-29 00:32:35 +0500

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -126,7 +126,7 @@ const App = () => {
             </p>
           </div>
 
-          <div className="container p-4 py-8 w-[90%] m-auto flex flex-wrap justify-center items-center gap-5">
+          <div className="container p-4 py-8 w-[50%] m-auto flex flex-wrap flex-row justify-center items-center gap-5">
             {projects.map((project) => (
               <ProjectCard
                 key={project.id}
