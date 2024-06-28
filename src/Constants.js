f2d94cb2f9bd6af19c4241e90f908f046840d6a2
Haider Ahmad <155129917+haider-9@users.noreply.github.com>
2024-06-29 00:33:30 +0500

    Add files via upload

--- src/Constants.js
+++ src/Constants.js
@@ -8,4 +8,12 @@ export const projects = [
     preview_url: "https://voyagertravels.vercel.app/",
     code_url: "https://github.com/haider-9",
   },
+  {
+    id: 2,
+    name: "Animadom",
+    description: "Welcome to Animadom, my first Next.js project.  ",
+    image_url: "../public/assets/animadom.png",
+    preview_url: "https://animadom.vercel.app/",
+    code_url: "https://github.com/haider-9",
+  },
 ];
